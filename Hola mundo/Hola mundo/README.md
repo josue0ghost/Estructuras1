d38024ac9dce52dcf7fc4a010711e0e46f9ead20
josue0ghost <36039766+josue0ghost@users.noreply.github.com>
2019-08-19 16:01:09 -0600

    First vue
    Type "cmd" in the explorer's search box in the project folder

---
+++ Hola mundo/Hola mundo/README.md
@@ -0,0 +1,3 @@
+﻿# Hola mundo
+
+
---
+++ Hola mundo/Hola mundo/src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+        <h1>{{ msg }}</h1>
+        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a> and <a href="http://www.typescriptlang.org/" target="_blank">TypeScript</a>.</p>
+        <span>contador: {{ contador }}</span>
+        <input type="button" @click ="increment" />
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Home extends Vue {
+        @Prop() private msg!: string;
+
+        contador: number = 0
+
+        increment() {
+            this.contador += 1
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
